4c903359cd69a7bab7e90eaa751af2640eea8637
Kabrax01 <rogalski.arnold@gmail.com>
2024-12-12 17:40:43 +0100

    feat: added delete request

--- server/server.js
+++ server/server.js
@@ -6,12 +6,15 @@ import Post from "./models/post.model.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.get("/api/", (req, res) => {
     res.send("server is ready");
 });
 
 app.post("/api/posts", async (req, res) => {
     const post = req.body;
+    console.log(req.body);
 
     if (!post.author || !post.title || !post.content || !post.createdAt) {
         return res
@@ -26,6 +29,18 @@ app.post("/api/posts", async (req, res) => {
         res.status(201).json({ success: true, data: newPost });
     } catch (error) {
         console.error(`Error in post request. Message: ${error.message}`);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
+
+app.delete("/api/posts/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        await Post.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Post deleted" });
+    } catch (error) {
+        console.error(`Error in delete request. Message: ${error.message}`);
         res.status(500).json({ success: false, message: "Server Error" });
     }
 });
