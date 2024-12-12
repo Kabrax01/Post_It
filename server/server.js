2b8c20bbab63d67df1b440fca3c8c86758d9bbac
Kabrax01 <rogalski.arnold@gmail.com>
2024-12-12 17:08:55 +0100

    feat: added post method

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import { connectDB } from "./config/db.js";
+import Post from "./models/post.model.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +10,27 @@ app.get("/api/", (req, res) => {
     res.send("server is ready");
 });
 
+app.post("/api/posts", async (req, res) => {
+    const post = req.body;
+
+    if (!post.author || !post.title || !post.content || !post.createdAt) {
+        return res
+            .status(400)
+            .json({ success: false, message: "Please provide all fields" });
+    }
+
+    const newPost = new Post(post);
+
+    try {
+        await newPost.save();
+        res.status(201).json({ success: true, data: newPost });
+    } catch (error) {
+        console.error(`Error in post request. Message: ${error.message}`);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
+
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}/api/`);
     connectDB();
+    console.log(`Server is running at http://localhost:${port}/api/`);
 });
