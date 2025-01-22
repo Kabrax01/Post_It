f2f2772df433262e4061202e924fa75154a65af3
Kabrax01 <rogalski.arnold@gmail.com>
2025-01-22 16:48:46 +0100

    feat: added mongo image to docker

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const conn = await mongoose.connect(process.env.DOCKER_MONGODB_URL);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(`MongoDB error ${error.message}`);
