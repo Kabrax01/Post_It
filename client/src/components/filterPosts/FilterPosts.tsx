96698f0ff2da30dbcf8b45405097cb47befeada6
Kabrax01 <rogalski.arnold@gmail.com>
2025-02-12 19:28:19 +0100

    feat: filterPosts component part 2

--- client/src/components/filterPosts/FilterPosts.tsx
+++ client/src/components/filterPosts/FilterPosts.tsx
@@ -1,20 +1,59 @@
 import styles from "./filterPosts.module.scss";
+import { FilterPostProps, FilterType } from "../../entities/types";
+import { useEffect, useState } from "react";
 
-const FilterPosts = () => {
+const FilterPosts = ({ posts, setFilteredPosts }: FilterPostProps) => {
+    const [filterType, setFilterType] = useState<FilterType>("author");
+    const [filterDate, setFilterDate] = useState("newest");
+    const [filterString, setFilterString] = useState("");
+
+    useEffect(() => {
+        if (filterString === "") setFilteredPosts(null);
+
+        const postsCopy = [...posts];
+
+        const filteredPosts = postsCopy.filter((post) =>
+            post[filterType].toLowerCase().includes(filterString.toLowerCase())
+        );
+
+        setFilteredPosts(filteredPosts);
+
+        //   2691978
+    }, [filterString, filterType, posts, setFilteredPosts]);
+
     return (
         <div className={styles.container}>
-            <input type="text" placeholder="Search..." />
+            <input
+                type="text"
+                placeholder="Search..."
+                value={filterString}
+                onChange={(e) => setFilterString(() => e.target.value)}
+            />
             <div className={styles.select_container}>
-                <label htmlFor="filter_type">Search by:</label>
-                <select name="posts-filters" id="filter_type">
-                    <option value="date-newest">Author</option>
-                    <option value="date-oldest">Title</option>
-                </select>
-                <label htmlFor="filter_by_date">Date: </label>
-                <select name="posts-filters" id="filter_by_date">
-                    <option value="date-newest">Newest</option>
-                    <option value="date-oldest">Oldest</option>
-                </select>
+                <label htmlFor="filter_type">
+                    Search by:
+                    <select
+                        name="posts-filters"
+                        id="filter_type"
+                        value={filterType}
+                        onChange={(e) =>
+                            setFilterType(e.target.value as FilterType)
+                        }>
+                        <option value="author">Author</option>
+                        <option value="title">Title</option>
+                    </select>
+                </label>
+                <label htmlFor="filter_by_date">
+                    Date:
+                    <select
+                        name="posts-filters"
+                        id="filter_by_date"
+                        value={filterDate}
+                        onChange={(e) => setFilterDate(e.target.value)}>
+                        <option value="newest">Newest</option>
+                        <option value="oldest">Oldest</option>
+                    </select>
+                </label>
             </div>
         </div>
     );
--- client/src/components/postsList/PostsList.tsx
+++ client/src/components/postsList/PostsList.tsx
@@ -2,26 +2,34 @@ import styles from "./postList.module.scss";
 import Post from "../post/Post";
 import { useStoreSubscribe } from "../../hooks/useStoreSubscribe";
 import FilterPosts from "../filterPosts/FilterPosts";
+import { useState } from "react";
+import { Post as PostType } from "../../entities/types";
 
 const PostsList = () => {
+    const [filteredPosts, setFilteredPosts] = useState<null | PostType[]>(null);
+
     const posts = useStoreSubscribe("posts");
     const loading = useStoreSubscribe("loading");
+
+    const postsToDisplay = !filteredPosts ? posts : filteredPosts;
 
     if (loading)
         return (
             <div className={styles.loader_container}>
-                Loading... <span className={styles.loader}></span>{" "}
+                Loading... <span className={styles.loader}></span>
             </div>
         );
 
     return (
         <div className={styles.posts_container}>
-            <FilterPosts />
+            <FilterPosts posts={posts} setFilteredPosts={setFilteredPosts} />
             <ul className={styles.posts_list}>
                 {!posts.length ? (
                     <p className={styles.placeholder}>No posts to display...</p>
                 ) : (
-                    posts.map((post) => <Post post={post} key={post.id} />)
+                    postsToDisplay.map((post) => (
+                        <Post post={post} key={post.id} />
+                    ))
                 )}
             </ul>
         </div>
