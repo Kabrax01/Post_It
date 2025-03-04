6c6b06c620022fce275bf9dc2664a5914108a229
Kabrax01 <rogalski.arnold@gmail.com>
2025-03-04 15:52:58 +0100

    fix: fixed TS types in tests

--- client/src/components/addPost/addPost.test.tsx
+++ client/src/components/addPost/addPost.test.tsx
@@ -1,5 +1,6 @@
 import { it, expect, describe } from "vitest";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import AddPost from "./AddPost";
 
 describe("addPost", () => {
