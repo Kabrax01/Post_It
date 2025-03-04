1a0757e977e19b51f727e066ed7cbba1d967f486
Kabrax01 <rogalski.arnold@gmail.com>
2025-03-04 16:58:02 +0100

    feat: added initial addPost tests

--- client/src/components/addPost/addPost.test.tsx
+++ client/src/components/addPost/addPost.test.tsx
@@ -1,5 +1,6 @@
 import { it, expect, describe } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import AddPost from "./AddPost";
 
@@ -9,12 +10,34 @@ describe("addPost", () => {
 
         return {
             heading: screen.getByRole("heading"),
+            button: screen.getByTestId("open form button"),
+            form: screen.queryByTestId("form"),
+            user: userEvent.setup(),
         };
     };
 
-    it("should render heading", () => {
-        const { heading } = renderComponent();
+    it("should render heading and button", () => {
+        const { heading, button } = renderComponent();
 
         expect(heading).toBeInTheDocument();
+        expect(button).toBeInTheDocument();
+    });
+
+    it("from should not be visible on the initial render", () => {
+        const { form } = renderComponent();
+
+        expect(form).not.toBeVisible();
+    });
+
+    it("should show and hide form after user clicks the button ", async () => {
+        const { form, button, user } = renderComponent();
+
+        await user.click(button);
+
+        expect(form).toBeVisible();
+
+        await user.click(button);
+
+        expect(form).not.toBeVisible();
     });
 });
